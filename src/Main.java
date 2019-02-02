89bf58078752096f9d798210006a4624109dfcad
mitodz <mitodzzz@gmail.com>
2019-02-02 10:12:44 +0300

    Код почти работает =)

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,10 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
-    static String getFinalListOfSortedLetters (Queue<Node> q) {
-        Queue<Node> qClone = new PriorityQueue<>(q);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < q.size(); i++) {
-            sb.append(qClone.poll().getLetter());
-        }
-        return sb.reverse().toString();
-    }
 
-    static void printTree(String s, String st, Node tree) {
-        int n = st.length();
+    static void printTree(String s, Node tree) {
+        String st = tree.getLetter(); //финальный отсортированный список букв
+        int n = st.length(); //количество уникальных букв
         System.out.println(n + " " + tree.getBits()); //реализация вывода уникальных букв и количества бит
         for (int i = 0; i < n; i++) { //реализация кода на каждую букву
             System.out.println(st.charAt(i) + ": " + tree.writeCode(Character.toString(st.charAt(i))));
@@ -30,7 +23,7 @@ public class Main {
         Comparator<Node> comparator = Comparator.comparing(Node::getCount).thenComparing(comparatorString);
         Queue<Node> q = new PriorityQueue<>(comparator);
 
-        Scanner scanner = new Scanner("abacabaddddddd");
+        Scanner scanner = new Scanner("abacabadddd");
         String s = scanner.nextLine();
         long n = s.chars().distinct().count(); // количество уникальных букв
 
@@ -41,19 +34,19 @@ public class Main {
                 .sorted(Map.Entry.<Character, Long>comparingByValue().thenComparing(Map.Entry::getKey))
                 .forEach(x -> q.add(new Node(Character.toString(x.getKey()), x.getValue())));
 
-        String st = getFinalListOfSortedLetters(q); //финальный отсортированный список букв
         Node i;
         Node j;
         for (long k = n + 1; k <= 2 * n - 1; k++) {
             i = q.poll();
             j = q.poll();
-            Node p = new Node(q.isEmpty() && (n % 2) != 0 ? i.getLetter() + j.getLetter() :
+            boolean isEmptyAndAlphabeticCompare = q.isEmpty() && (i.getLetter().compareToIgnoreCase(j.getLetter())>0);
+            Node p = new Node(isEmptyAndAlphabeticCompare ? i.getLetter() + j.getLetter() :
                     j.getLetter() + i.getLetter(), i.getCount() + j.getCount());
-            p.setLeft(q.isEmpty() && (n % 2) != 0 ? j : i);
-            p.setRight(q.isEmpty() && (n % 2) != 0 ? i : j);
+            p.setLeft(isEmptyAndAlphabeticCompare ? j : i);
+            p.setRight(isEmptyAndAlphabeticCompare ? i : j);
             q.add(p);
         }
         Node tree = q.poll();
-        printTree(s,st,tree);
+        printTree(s,tree);
     }
 }
