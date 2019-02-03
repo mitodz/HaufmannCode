930504d2ac9a38358eafc77023db0d062bef71d9
mitodz <mitodzzz@gmail.com>
2019-02-03 14:28:50 +0300

    Кажется, работает....

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ import java.util.stream.Collectors;
 
 public class Main {
 
-    static void printTree(String s, Node tree) {
-        String st = tree.getLetter(); //финальный отсортированный список букв
+    static void printTree(String s, String st, Node tree) {
         int n = st.length(); //количество уникальных букв
         System.out.println(n + " " + tree.getBits()); //реализация вывода уникальных букв и количества бит
         for (int i = 0; i < n; i++) { //реализация кода на каждую букву
@@ -19,32 +18,40 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Comparator<Node> comparatorString = Comparator.comparing(Node::getLetter, Comparator.comparing(x -> x.charAt(0),Character::compareTo)).reversed();//
-        Comparator<Node> comparator = Comparator.comparing(Node::getCount,Long::compareTo).thenComparing(comparatorString);
+        Comparator<Node> comparatorString = Comparator.comparing(Node::getLetter, Comparator.comparing(x -> x.charAt(0), Character::compareTo)).reversed();//
+        Comparator<Node> comparator = Comparator.comparing(Node::getCount, Long::compareTo).thenComparing(comparatorString);
         Queue<Node> q = new PriorityQueue<>(comparator);
 
-        Scanner scanner = new Scanner("aaaaaaaaaaaaaaabbbbbbbccccccddddddeeeee");//aaaaaaaaaaaaaaabbbbbbbccccccddddddeeeee
+        Scanner scanner = new Scanner("abacabad");//aaaaaaaaaaaaaaabbbbbbbccccccddddddeeeee
         String s = scanner.nextLine();
+        StringBuilder sb = new StringBuilder();
         long n = s.chars().distinct().count(); // количество уникальных букв
+        if (n == 1) {
+            System.out.print("1 1\n" + s + ": 0\n0");
+        } else {
+            s.chars().mapToObj(x -> (char) x)
+                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                    .entrySet()
+                    .stream()
+                    .sorted(Map.Entry.<Character, Long>comparingByValue().thenComparing(Map.Entry::getKey))
+                    .forEach(x -> {
+                        sb.append(x.getKey());
+                        q.add(new Node(Character.toString(x.getKey()), x.getValue()));
+                    });
+            String st = sb.reverse().toString();//финальный отсортированный список букв
 
-        s.chars().mapToObj(x -> (char) x)
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet()
-                .stream()
-                .sorted(Map.Entry.<Character, Long>comparingByValue().thenComparing(Map.Entry::getKey))
-                .forEach(x -> q.add(new Node(Character.toString(x.getKey()), x.getValue())));
-
-        Node i;
-        Node j;
-        for (long k = n + 1; k <= 2 * n - 1; k++) {
-            i = q.poll();
-            j = q.poll();
-            Node p = new Node(j.getLetter() + i.getLetter(), i.getCount() + j.getCount());
-            p.setLeft(i);
-            p.setRight(j);
-            q.offer(p);
+            Node i;
+            Node j;
+            for (long k = n + 1; k <= 2 * n - 1; k++) {
+                i = q.poll();
+                j = q.poll();
+                Node p = new Node(j.getLetter() + i.getLetter(), i.getCount() + j.getCount());
+                p.setLeft(i);
+                p.setRight(j);
+                q.offer(p);
+            }
+            Node tree = q.poll();
+            printTree(s, st, tree);
         }
-        Node tree = q.poll();
-        printTree(s,tree);
     }
 }
